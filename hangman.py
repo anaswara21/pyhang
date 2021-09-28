88abe951d591a37ac42a70cfbafd4f9aac93ef5e
anaswara21 <67572228+anaswara21@users.noreply.github.com>
2021-09-28 17:58:21 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,7 +4,7 @@ from random import choice
 def cls():
     os.system("cls")
 
-
+#function to make gibbet
 def hang(status):
     if status==0:
         print('  ----------')
@@ -76,61 +76,91 @@ def hang(status):
         print('  |         |') 
         print('  |        / \\') 
         print('  |')
-        print('  |_________________')     
-                 
+        print('  |_________________') 
 
-words=["apple","mango"]
-random_word=choice(words)
+# function for hint 
+              
+def hint(status):
+    if(status==3):
+        print()
+        print("\t\t*********** Hint ***********")
+        print("\t\tIt is a name of an animal !!")
+        print("\t\t****************************")
 
-word_list=[]
+ #  function for display the score board
+     
+def score_board(score):
+    print("*---------------------------*")
+    print("|--------SCORE BOARD--------|")
+    print("|---------------------------|")
+    print(f'|\t  SCORE::{score}\t    |')
+    print("*---------------------------*")
+
+# empty dictionary 
+words = {}  
+file=open('animal.txt','r')
+for i in file.readlines():
+    i=i.replace('\n','')
+    number,animal=i.split('.')
+    words[number]=animal
+file.close
+random_word=choice(list(words.values()))
+
+#list to store the actual word
+word_list = []
 for i in random_word:
     word_list.append(i)
  
-
-hide=[]
+#list to store the word hidden
+hide = []
 for i in range(len(random_word)):
     hide.append('___')
-
+print()
 print("########################")
-print("######HANGMAN GAME######")
+print("##### HANGMAN GAME #####")
 print("########################\n")
 
 print("INSTRUCTIONS")
 print("-------------")
-print("1.you can use either upper or lower case letters(A and a are same)\n")
-print("2.you have to type the letter in a word as many times as it is repeated in that word\neg:-word:APPLE ,you have to enter P two times\n")
-print("3.after three wrong choice ,we will provide a hint for you\n")
-print("4.you can make wrong choice only 5 times.after that game will terminated\n")
-print("5.guess the word before your man gets hung!\n")
-print("Are you ready to play?\n")
-name=input("Enter your name:")
-input("just press the enter to start  :")
+print("1. You can use either upper or lower case letters(A and a are same).\n")
+print("2. You have to type the letter in a word as many times as it is repeated in that word\n   Eg:-\n   Word: APPLE, you have to enter P two times.\n")
+print("3. After three wrong choice ,we will provide a hint for you.\n")
+print("4. You can make wrong choice only 5 times.after that game will terminated.")
+print("5. Guess the word before your man gets hung!\n")
+print("Get Ready Folks !!\n")
+name=input("Enter your name : ")
+input("Just Press The Enter To Start !")
 cls()
-error=[]
-while len(error)<=5:
-    hang(len(error))
+
+#list to store the wrong choices
+wrong_choices = []
+score = 0
+while len(wrong_choices)<=5:
+    hang(len(wrong_choices))
+    hint(len(wrong_choices))
     print("\n")
+    score_board(score)
     print(f'correct word:{" ".join(hide)}\n')
-    print(f'wrong inputs:{",".join(error)}\n')
-    a=input("input the letter:")
-    a=a.lower()
-    if a in word_list:
-        hide[word_list.index(a)]=a
-        word_list[word_list.index(a)]=" "
+    print(f'wrong inputs:{",".join(wrong_choices)}\n')
+    letter=input("input the letter:")
+    letter=letter.lower()
+    if letter in word_list:
+        hide[word_list.index(letter)]=letter
+        word_list[word_list.index(letter)]=" "
+        score +=1
     else:
-        error.append(a)
+        wrong_choices.append(letter)
         print(f'it was a wrong choice')
     if random_word == ''.join(hide):
         cls()
         print("congratulations "+name)
         print('You won!')
+        score_board(score)
         break
     cls()
 else:
-    hang(len(error))
-    print(f'you lose ,try again/n')
-    print(f'The word was {random_word}')
-        
-
+    hang(len(wrong_choices))
+    print(f'you lose ,try again\n')
+    print(f'The word was {random_word}\n')
         
 
